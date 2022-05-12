68b51ad6437023a6405a90ab80d897b660c2d0d9
NaufalHisyam18 <naufalhisyam84@gmail.com>
2022-05-12 14:04:03 +0700

    membuat splash screen dan login screen

---
+++ jobqo_mobile/lib/screen/splashScreen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:jobqo_mobile/screen/loginScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  splashscreenStart() async {
+    var duration = const Duration(seconds: 2);
+    return Timer(duration, () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 32, 70, 116),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset("images/logo.png"),
+          SizedBox(
+            height: 24.0,
+          ),
+        ],
+      )),
+    );
+  }
+}
